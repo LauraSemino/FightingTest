ebda627f8d5f904f01589d2174a55de26cb2b17d
LauraSemino <seminol@sheridancollege.ca>
2025-07-05 15:19:55 -0400

    Directional Inputs Functional

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,16 +5,19 @@ using UnityEditor;
 using System;
 using NodeCanvas.Tasks.Actions;
 using System.Linq;
+using ParadoxNotion.Serialization.FullSerializer;
 
 public class Move : MonoBehaviour
 {
 
     public Rigidbody rb;
     public Vector3 localVel;
-    public Vector3 moveInput;
+    public Vector2 moveInput;
     public float walkSpeed;
+    public float buttonHeldTimer;
 
-   
+    public Vector3 lastInput;
+    public Vector3 currentInput;
     public List<Vector3> inputHistory;
     // Start is called before the first frame update
     void Start()
@@ -26,50 +29,93 @@ public class Move : MonoBehaviour
     void Update()
     {
         moveInput = Vector3.zero;
+        lastInput = currentInput;
+
+        buttonHeldTimer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            buttonHeldTimer = 0;
+            inputHistory.Add(lastInput);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            buttonHeldTimer = 0;
+            inputHistory.Add(lastInput);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            buttonHeldTimer = 0;
+            inputHistory.Add(lastInput);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            buttonHeldTimer = 0;
+            inputHistory.Add(lastInput);
+        }
+
+
         if (Input.GetKey(KeyCode.A))
         {
             moveInput.x = -1;
+            //buttonHeldTimer += Time.deltaTime;
+            currentInput = (new Vector3(moveInput.x, moveInput.y, buttonHeldTimer));
         }
         if (Input.GetKey(KeyCode.D))
         {
             moveInput.x = 1;
+            //buttonHeldTimer += Time.deltaTime;
+            currentInput = (new Vector3(moveInput.x, moveInput.y, buttonHeldTimer));
         }
         if (Input.GetKey(KeyCode.W))
         {
             moveInput.y = 1;
+            //buttonHeldTimer += Time.deltaTime;
+            currentInput = (new Vector3(moveInput.x, moveInput.y, buttonHeldTimer));
         }
         if (Input.GetKey(KeyCode.S))
         {
             moveInput.y = -1;
+            //buttonHeldTimer += Time.deltaTime;
+            currentInput = (new Vector3(moveInput.x, moveInput.y, buttonHeldTimer));
+        }
+        if (moveInput == Vector2.zero)
+        {
+            currentInput = (new Vector3(moveInput.x, moveInput.y, buttonHeldTimer));
+           
         }
 
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyUp(KeyCode.A))
         {
-            inputHistory.Add(moveInput);
+            inputHistory.Add(lastInput);          
+            buttonHeldTimer = 0;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyUp(KeyCode.D))
         {
-            inputHistory.Add(moveInput);
+            inputHistory.Add(lastInput);
+            buttonHeldTimer = 0;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKeyUp(KeyCode.W))
         {
-            inputHistory.Add(moveInput);
+            inputHistory.Add(lastInput);
+            buttonHeldTimer = 0;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.S))
         {
-            inputHistory.Add(moveInput);
+            inputHistory.Add(lastInput);
+            buttonHeldTimer = 0;
         }
-        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-        {
-            inputHistory.Add(moveInput);
-        }
+
+
         
 
         if (inputHistory.Count > 30)
         {
+            
             inputHistory.RemoveAt(0);
         }
 
+
+        
     }
    
 
@@ -78,9 +124,7 @@ public class Move : MonoBehaviour
         localVel = rb.velocity;
 
         localVel.x = moveInput.x * walkSpeed;
-             
-        
-
+                    
         rb.velocity = localVel;
     }
 }
