5fce0e612ec4b51aaf81837d0c8f1f2df58ba582
LauraSemino <seminol@sheridancollege.ca>
2025-07-17 17:38:59 -0400

    Camera works

--- Assets/Scripts/CamManager.cs
+++ Assets/Scripts/CamManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CamManager : MonoBehaviour
 {
+    public GameObject p1;
+    public GameObject p2;
+    
+    public float stageLength;
+    public Vector3 camOffset;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,21 @@ public class CamManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //float pDist = Vector3.Distance(p1.transform.position, p2.transform.position);
+
+
+        if (transform.position.x <= stageLength && transform.position.x >= -stageLength)
+        {
+            transform.position = new Vector3((p1.transform.position.x + p2.transform.position.x) / 2, camOffset.y, camOffset.z);
+        }
+        if(transform.position.x >= stageLength)
+        {
+            transform.position = new Vector3 (stageLength, camOffset.y,camOffset.z);
+        }
+        if (transform.position.x <= -stageLength)
+        {
+            transform.position = new Vector3(-stageLength, camOffset.y, camOffset.z);
+        }
+
     }
 }
